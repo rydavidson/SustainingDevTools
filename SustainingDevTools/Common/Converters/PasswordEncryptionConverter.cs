3bc1ec4fea53c23acb964b4ca041348a931a8ba4
Ryan Davidson <rydavidson@accela.com>
2018-11-08 08:17:03 -0700

    Added password encryption converter

---
+++ SustainingDevTools/Common/Converters/PasswordEncryptionConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using SustainingDevTools.Common;
+
+namespace SustainingDevTools.Common.Converters
+{
+    public class PasswordEncryptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return SecureUtils.Protect(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return SecureUtils.Unprotect(value.ToString());
+        }
+    }
+}
